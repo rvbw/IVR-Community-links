3e21c05f1e94d715814402936f1b7d16bac68e11
ALrahall <bandr763355@gmail.com>
2026-07-08 09:43:49 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+particlesJS("particles-js", {
+
+    particles: {
+
+        number: {
+            value: 45
+        },
+
+        color: {
+            value: "#12A8C7"
+        },
+
+        shape: {
+            type: "circle"
+        },
+
+        opacity: {
+            value: 0.3
+        },
+
+        size: {
+            value: 2
+        },
+
+        move: {
+
+            enable: true,
+
+            speed: 0.8
+
+        },
+
+        line_linked:{
+
+            enable:true,
+
+            color:"#12A8C7",
+
+            opacity:0.08,
+
+            distance:180
+
+        }
+
+    }
+
+});
